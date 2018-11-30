9ada08a63b182a63b6f1f12f9146a2861fc2fab2
jay <jay.liu@shinesolutions.com>
2018-11-30 14:00:31 +1100

    add UserService test

---
+++ src/test/java/com/shineSolutions/nabPreparation/service/TestUserService.java
@@ -0,0 +1,40 @@
+package com.shineSolutions.nabPreparation.service;
+
+import com.shineSolutions.nabPreparation.model.TransactionsEntity;
+import com.shineSolutions.nabPreparation.model.UsersEntity;
+import com.shineSolutions.nabPreparation.repository.TransactionRepositoryImp;
+import com.shineSolutions.nabPreparation.repository.UserRepositoryImp;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class TestUserService {
+
+    @Autowired
+    protected TransactionRepositoryImp transRepo;
+
+    @Autowired
+    protected UserRepositoryImp usersRepo;
+
+    @Test
+    public void shouldFGetAllTransByUserId() {
+        Collection<TransactionsEntity> entities = this.transRepo.findAllByUserId(1l);
+        assertThat(entities.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void shouldFGetAllUsers() {
+        Collection<UsersEntity> users = this.usersRepo.findUsers();
+        assertThat(users.size()).isEqualTo(3);
+    }
+}
