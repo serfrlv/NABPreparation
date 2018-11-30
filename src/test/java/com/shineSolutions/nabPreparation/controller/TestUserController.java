cba6e467b603d5597925ff9ef41d46c66d18d3ed
jay <jay.liu@shinesolutions.com>
2018-11-30 14:00:00 +1100

    add UserController test

--- src/test/java/com/shineSolutions/nabPreparation/controller/TestUserController.java
+++ src/test/java/com/shineSolutions/nabPreparation/controller/TestUserController.java
@@ -4,31 +4,75 @@ import com.shineSolutions.nabPreparation.model.TransactionDTO;
 import com.shineSolutions.nabPreparation.model.UserDTO;
 import com.shineSolutions.nabPreparation.service.UserService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(UserController.class)
 public class TestUserController {
 
+    private static final Long TEST_USER_ID = 1L;
+
     @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private UserService userService;
 
+    @Before
+    public void init() {
+        UserDTO u1 = UserDTO.builder().userName("James.Bond").userId(1l).build();
+        UserDTO u2 = UserDTO.builder().userName("James.Bond").userId(1l).build();
+        UserDTO u3 = UserDTO.builder().userName("James.Bond").userId(1l).build();
+        List<UserDTO> users = new ArrayList<>();
+        users.add(u1);
+        users.add(u2);
+        users.add(u3);
+        TransactionDTO t1 = TransactionDTO.builder()
+                .originUserId(1l).originUserName("James.Bond")
+                .targetUserId(2l).targetUserName("Tom.Hanks")
+                .amount(50).transDate(LocalDate.parse("2018-11-04")).build();
+        List<TransactionDTO> trans = new ArrayList<>();
+        trans.add(t1);
+        given(this.userService.findTransactionsByUserId(TEST_USER_ID)).willReturn(trans);
+        given(this.userService.findAllUsers()).willReturn(users);
+    }
+
     @Test
-    public void TestGetUsers() throws Exception{
+    public void testGetUsers(){
         List<UserDTO> users = userService.findAllUsers();
         Assert.assertEquals(3,users.size());
         Assert.assertEquals("James.Bond",users.get(0).getUserName());
     }
     @Test
-    public void TestGetTransactions() throws Exception{
-        List<TransactionDTO> transactionDTOS = userService.findTransactionsByUserId(new Long(2));
-        Assert.assertEquals(1,transactionDTOS.size());
+    public void testGetTransactions(){
+        List<TransactionDTO> trans = userService.findTransactionsByUserId(new Long(1));
+        Assert.assertEquals(1,trans.size());
+        Assert.assertEquals(50,trans.get(0).getAmount(),0.0);
     }
 
+    @Test
+    public void testGetTransactionByUserSuccessful() throws Exception {
+        mockMvc.perform(get("/user/{userId}/transactions", TEST_USER_ID))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void testGetUsersSuccessful() throws Exception {
+        mockMvc.perform(get("/user/"))
+                .andExpect(status().isOk());
+    }
 }
