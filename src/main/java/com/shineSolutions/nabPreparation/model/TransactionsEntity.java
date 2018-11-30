1542294224dfcd9d411d84cc6168532b34184617
jay <jay.liu@shinesolutions.com>
2018-11-30 12:09:09 +1100

    using lombok for the boilerplate setter and getter

--- src/main/java/com/shineSolutions/nabPreparation/model/TransactionsEntity.java
+++ src/main/java/com/shineSolutions/nabPreparation/model/TransactionsEntity.java
@@ -10,9 +10,12 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name="transactions")
-
+@Getter
+@Setter
+@ToString
 public class TransactionsEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
@@ -33,60 +36,4 @@ public class TransactionsEntity {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public Long getTargetUserId() {
-        return targetUserId;
-    }
-
-    public void setTargetUserId(Long targetUserId) {
-        this.targetUserId = targetUserId;
-    }
-
-    public String getTargetUserName() {
-        return targetUserName;
-    }
-
-    public void setTargetUserName(String targetUserName) {
-        this.targetUserName = targetUserName;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
 }
