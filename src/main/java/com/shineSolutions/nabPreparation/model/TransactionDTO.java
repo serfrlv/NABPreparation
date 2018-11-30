1cab57da9e24754b91a00b54a2ccd863af92a54d
jay <jay.liu@shinesolutions.com>
2018-11-30 12:08:24 +1100

    using lombok for the boilerplate setter and getter

--- src/main/java/com/shineSolutions/nabPreparation/model/TransactionDTO.java
+++ src/main/java/com/shineSolutions/nabPreparation/model/TransactionDTO.java
@@ -1,7 +1,16 @@
 package com.shineSolutions.nabPreparation.model;
 
-import java.util.Date;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@ToString
+@Builder
 public class TransactionDTO {
 
     private Long originUserId;
@@ -9,53 +18,5 @@ public class TransactionDTO {
     private Long targetUserId;
     private String targetUserName;
     private double amount;
-    private Date transDate;
-
-    public Long getOriginUserId() {
-        return originUserId;
-    }
-
-    public void setOriginUserId(Long originUserId) {
-        this.originUserId = originUserId;
-    }
-
-    public String getOriginUserName() {
-        return originUserName;
-    }
-
-    public void setOriginUserName(String originUserName) {
-        this.originUserName = originUserName;
-    }
-
-    public Long getTargetUserId() {
-        return targetUserId;
-    }
-
-    public void setTargetUserId(Long targetUserId) {
-        this.targetUserId = targetUserId;
-    }
-
-    public String getTargetUserName() {
-        return targetUserName;
-    }
-
-    public void setTargetUserName(String targetUserName) {
-        this.targetUserName = targetUserName;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public Date getTransDate() {
-        return transDate;
-    }
-
-    public void setTransDate(Date transDate) {
-        this.transDate = transDate;
-    }
+    private LocalDate transDate;
 }
