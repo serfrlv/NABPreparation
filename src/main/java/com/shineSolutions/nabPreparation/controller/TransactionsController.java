9ed13e64cbfe2db7a2e8c70b6fbfd2d5e3e8c146
jay <jay.liu@shinesolutions.com>
2018-12-03 12:17:07 +1100

    init TransactionsController

---
+++ src/main/java/com/shineSolutions/nabPreparation/controller/TransactionsController.java
@@ -0,0 +1,19 @@
+package com.shineSolutions.nabPreparation.controller;
+
+
+import com.shineSolutions.nabPreparation.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/transactions")
+public class TransactionsController {
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping(value="/")
+    public Iterable getAllTransactions(){
+        return userService.findAllTransactions();
+    }
+}
