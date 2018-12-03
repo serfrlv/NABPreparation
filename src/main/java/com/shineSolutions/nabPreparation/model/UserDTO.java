aabd29a4f9d0f66507ab1cff4bd4af5ddecaff31
jay <jay.liu@shinesolutions.com>
2018-12-03 11:27:55 +1100

    using lombok @Data annotation

--- src/main/java/com/shineSolutions/nabPreparation/model/UserDTO.java
+++ src/main/java/com/shineSolutions/nabPreparation/model/UserDTO.java
@@ -1,13 +1,8 @@
 package com.shineSolutions.nabPreparation.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Getter
-@Setter
-@ToString
+@Data
 @Builder
 public class UserDTO {
 
