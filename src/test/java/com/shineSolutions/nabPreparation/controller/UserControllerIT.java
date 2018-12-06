577f76ef1b5e92218a3eae194cac972cd88cd2eb
jay <jay.liu@shinesolutions.com>
2018-12-06 18:05:46 +1100

    add the integration test

---
+++ src/test/java/com/shineSolutions/nabPreparation/controller/UserControllerIT.java
@@ -0,0 +1,70 @@
+package com.shineSolutions.nabPreparation.controller;
+
+
+import com.shineSolutions.nabPreparation.model.UserDTO;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerIT {
+
+    @LocalServerPort
+    int port;
+
+    private UserDTO userDTO;
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+        userDTO = UserDTO.builder().userName("Danny.Morphy").userId(5l).build();
+    }
+
+    @Test
+    public void getUserReturn200() throws InterruptedException {
+        given().basePath("/user").get("/")
+                .then()
+                .statusCode(200)
+                .body("userId", hasItems(1,2,3))
+                .body("userName",hasItems("James.Bond","Jimmy.White"));
+    }
+
+    @Test
+    public void getTransactionsReturn200() throws InterruptedException {
+        given().basePath("/transactions").get("/")
+                .then()
+                .statusCode(200)
+                .body("originUserId", hasItems(1,1,2))
+                .body("originUserName",hasItems("James.Bond"));
+    }
+
+    @Test
+    public void postUserReturn200() throws InterruptedException{
+        given().contentType("application/json")
+                .body(userDTO).contentType(ContentType.JSON)
+                .when().post("/user/new")
+                .then().statusCode(HttpStatus.SC_OK);
+    }
+    @Test
+    public void addUserShouldReturnItem(){
+        given()
+                .body(userDTO)
+                .contentType(ContentType.JSON)
+                .when().post("/user/new")
+                .then().statusCode(HttpStatus.SC_OK)
+                .body("userName",is("Danny.Morphy"));
+    }
+
+
+
+}
