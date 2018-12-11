b93b1bef48f26731b1b22f16911636457f5ed6f2
jay <jay.liu@shinesolutions.com>
2018-12-11 15:52:14 +1100

    make userDTO extend ResourceSupport to generate HATEOAS

--- src/main/java/com/shineSolutions/nabPreparation/model/UserDTO.java
+++ src/main/java/com/shineSolutions/nabPreparation/model/UserDTO.java
@@ -1,12 +1,13 @@
 package com.shineSolutions.nabPreparation.model;
 
 import lombok.*;
+import org.springframework.hateoas.ResourceSupport;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
+public class UserDTO  extends ResourceSupport {
 
     private Long  userId;
     private String userName;
