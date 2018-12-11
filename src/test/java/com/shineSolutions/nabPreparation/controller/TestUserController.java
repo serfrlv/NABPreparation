1e905e6d9e3f6cc403e726ae4fb128f86597af88
jay <jay.liu@shinesolutions.com>
2018-12-11 15:58:20 +1100

    add more test case

--- src/test/java/com/shineSolutions/nabPreparation/controller/TestUserController.java
+++ src/test/java/com/shineSolutions/nabPreparation/controller/TestUserController.java
@@ -1,10 +1,14 @@
 package com.shineSolutions.nabPreparation.controller;
 
+import com.shineSolutions.nabPreparation.exception.UserNotFoundException;
+import com.shineSolutions.nabPreparation.model.TransactionDTO;
+import com.shineSolutions.nabPreparation.model.UserDTO;
 import com.shineSolutions.nabPreparation.service.UserService;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,9 +16,19 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -31,44 +45,75 @@ public class TestUserController {
 
     @Before
     public void init() {
-//        List<UserDTO> users = new ArrayList<>();
-//        UserDTO u1 = UserDTO.builder().userName("James.Bond").userId(1l).build();
-//        users.add(u1);
-//        UserDTO u2 = UserDTO.builder().userName("Tom.Hanks").userId(2l).build();
-//        users.add(u2);
-//        UserDTO u3 = UserDTO.builder().userName("Jimmy.White").userId(3l).build();
-//        users.add(u3);
-//        TransactionDTO t1 = TransactionDTO.builder()
-//                .originUserId(1l).originUserName("James.Bond")
-//                .targetUserId(2l).targetUserName("Tom.Hanks")
-//                .amount(BigDecimal.valueOf(50)).transDate(LocalDate.parse("2018-11-04")).build();
-//        List<TransactionDTO> trans = new ArrayList<>();
-//        trans.add(t1);
-//        given(this.userService.findTransactionsByUserId(TEST_USER_ID)).willReturn(trans);
-//        given(this.userService.findAllUsers()).willReturn(users);
-//        UserDTO newUser = UserDTO.builder().userName("David.Jones").build();
-//        given(this.userService.addUser(newUser)).willReturn(newUser);
+        UserDTO u1 = UserDTO.builder().userName("James.Bond").userId(1l).build();
+        UserDTO u2 = UserDTO.builder().userName("Tom.Hanks").userId(2l).build();
+        UserDTO u3 = UserDTO.builder().userName("Jimmy.White").userId(3l).build();
+        List<UserDTO> users = Arrays.asList(u1,u2,u3);
+        TransactionDTO t1 = TransactionDTO.builder().originUserId(1l).originUserName("James.Bond")
+                .targetUserId(2l).targetUserName("Tom.Hanks").amount(BigDecimal.valueOf(50)).transDate(LocalDate.parse("2018-11-04")).build();
+        List<TransactionDTO> trans = Arrays.asList(t1);
+        given(this.userService.findAllUsers()).willReturn(users);
+        UserDTO newUser = UserDTO.builder().userName("David.Jones").build();
+        given(this.userService.addUser(UserDTO.builder().userName("David.Jones").build())).willReturn(newUser);
+        given(userService.findTransactionsByUserId(Mockito.anyLong())).willReturn(trans);
     }
 
     @Test
     public void testGetTransactionByUserSuccessful() throws Exception {
         mockMvc.perform(get("/user/{userId}/transactions", TEST_USER_ID))
-                .andExpect(status().isOk());
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("James.Bond")))
+                .andExpect(content().string(containsString("Tom.Hanks")));
+
     }
 
     @Test
     public void testGetUsersSuccessful() throws Exception {
         mockMvc.perform(get("/user/"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("James.Bond")))
+                .andExpect(content().string(containsString("Tom.Hanks")))
+                .andExpect(content().string(containsString("Jimmy.White")));
+
     }
 
     @Test
-    public void testAddUserSuccessful() throws Exception{
+    public void testGetUserNotFoundShouldException() throws Exception {
+        given(userService.findUserByUserId(6l)).willThrow(new UserNotFoundException("5"));
+        mockMvc.perform(get("/user/{userId}",5))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(status().reason(containsString("No such User")));
+
+    }
+
+    @Test
+    public void testPostUserSuccessful() throws Exception{
         String jason = "{\"userName\":\"Rooney.Wane\"}";
-        mockMvc.perform(post("/user/new/")
+        mockMvc.perform(post("/user")
                 .content(jason)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void testPutUserSuccessful() throws Exception{
+        String jason = "{\"userName\":\"Rooney.Wane\"}";
+        mockMvc.perform(put("/user/{userId}","4")
+                .content(jason)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteUserSuccessful() throws Exception{
+        mockMvc.perform(delete("/user/{userId}","3")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
 }
--- src/test/java/com/shineSolutions/nabPreparation/controller/UserControllerIT.java
+++ src/test/java/com/shineSolutions/nabPreparation/controller/UserControllerIT.java
@@ -13,6 +13,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.core.Is.is;
 
@@ -31,12 +32,27 @@ public class UserControllerIT {
     }
 
     @Test
-    public void getUserReturn200() throws InterruptedException {
+    public void getUserReturn200(){
         given().basePath("/user").get("/")
                 .then()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .body("userId", hasItems(1,2,3))
                 .body("userName",hasItems("James.Bond","Jimmy.White"));
+    }
+
+    @Test
+    public void getOneUserReturn200(){
+        given().contentType("application/json")
+                .when().get("/user/{userId}",1)
+                .then().statusCode(HttpStatus.SC_OK)
+                .body("userId", equalTo(1))
+                .body("userName",equalTo("James.Bond"));
+    }
+    @Test
+    public void getUserNotFoundShouldReturnUserNotFoundException(){
+        given().contentType("application/json")
+                .when().get("/user/{userId}",10)
+                .then().statusCode(HttpStatus.SC_NOT_FOUND);
     }
 
     @Test
@@ -49,22 +65,38 @@ public class UserControllerIT {
     }
 
     @Test
-    public void postUserReturn200() throws InterruptedException{
+    public void postUserReturn200AndShouldReturnItem() throws InterruptedException{
         given().contentType("application/json")
                 .body(userDTO).contentType(ContentType.JSON)
-                .when().post("/user/new")
-                .then().statusCode(HttpStatus.SC_OK);
+                .when().post("/user")
+                .then().statusCode(HttpStatus.SC_OK)
+                .body("userName",is("Danny.Morphy"));
     }
+
     @Test
     public void addUserShouldReturnItem(){
         given()
                 .body(userDTO)
                 .contentType(ContentType.JSON)
-                .when().post("/user/new")
+                .when().post("/user")
                 .then().statusCode(HttpStatus.SC_OK)
                 .body("userName",is("Danny.Morphy"));
     }
 
+    @Test
+    public void putUserReturn200(){
+        given().contentType("application/json")
+                .body(userDTO)
+                .contentType(ContentType.JSON)
+                .when().put("/user/{uerId}",4)
+                .then().statusCode(HttpStatus.SC_OK);
+    }
 
-
+    @Test
+    public void delUserReturn200(){
+        given().contentType("application/json")
+                .contentType(ContentType.JSON)
+                .when().delete("/user/{userId}",1)
+                .then().statusCode(HttpStatus.SC_OK);
+    }
 }
